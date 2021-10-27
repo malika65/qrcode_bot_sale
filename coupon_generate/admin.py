b3f892a59365bb05275473cbe8d85e050842cc3e
malika65 <malika.satimbaeva@iaau.edu.kg>
2021-10-27 13:06:41 +0600

    Add models and register them in admin

--- coupon_generate/admin.py
+++ coupon_generate/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Organization, QRCode
+
+
+admin.site.register(Organization)
+admin.site.register(QRCode)
+
--- coupon_generate/models.py
+++ coupon_generate/models.py
@@ -2,3 +2,28 @@ from django.db import models
 
 # Create your models here.
 
+class Organization(models.Model):
+    name = models.CharField(max_length=200,verbose_name='Название',null=True,blank=True)
+    description = models.TextField(max_length=3000,verbose_name='Описание',null=True,blank=True)
+
+   
+    class Meta:
+        verbose_name = 'Организация'
+        verbose_name_plural = 'Организации'
+
+    def __str__(self):
+        return self.name
+
+class QRCode(models.Model):
+    name = models.CharField(max_length=200,verbose_name='Название',null=True,blank=True)
+    description = models.TextField(max_length=3000,verbose_name='Описание',null=True,blank=True)
+    pic = models.FileField(null=True,blank=True, upload_to='images/')
+    organization = models.ForeignKey(Organization,null=True,blank=True,on_delete=models.CASCADE)
+
+
+    class Meta:
+        verbose_name = 'QRкод'
+        verbose_name_plural = 'QRкоды'
+    
+    def __str__(self):
+        return self.name
