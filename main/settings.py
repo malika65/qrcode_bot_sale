606bcca8002eb4227e51ced31f1bd23ac894046e
malika65 <malika.satimbaeva@iaau.edu.kg>
2021-11-10 13:26:18 +0600

    Settings for heroku deploy

--- main/settings.py
+++ main/settings.py
@@ -53,7 +53,12 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+
 ]
+ROOT_URLCONF = 'main.urls'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 ROOT_URLCONF = 'main.urls'
 
