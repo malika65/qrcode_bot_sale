e376071778706844cb2edc48ee8d50ed2aabfe36
malika65 <malika.satimbaeva@iaau.edu.kg>
2021-11-11 16:53:29 +0600

    Try to set static settings

--- main/settings.py
+++ main/settings.py
@@ -149,8 +149,8 @@ STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 STATIC_URL = '/static/'
 
 # location where you will store your static files
-STATIC_DIRS = [
-   ]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
